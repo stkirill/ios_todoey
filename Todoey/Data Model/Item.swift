c6a2fc9f37d3df53c9f8f2211b7ed5a8795a6c28
Kirill Strelkin <st.kirill@gmail.com>
2018-07-04 06:08:13 +0300

    Convert to MVC

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Kirill Strelkin on 03/07/2018.
+//  Copyright © 2018 Kirill Strelkin. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title : String = ""
+    var done : Bool = false
+    
+}
